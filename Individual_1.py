017e48290f6215503cf90171fb4548e114bea8e6
IsSveshuD <ivan.ivan.lysenko@gmail.com>
2021-05-28 03:23:04 +0300

    add all

---
+++ Individual_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#Ввести список А из 10 элементов, найти наименьший элемент и переставить его с
+# последним элементом. Преобразованный массив вывести.
+
+import sys
+
+if __name__ == '__main__':
+# Ввести список одной строкой.
+    a = list(map(int, input().split()))
+    n = 9
+# Если список пуст, завершить программу.
+    if len(a) != 10:
+        print("Неверный размер текста", file= sys.stderr)
+        exit(1)
+    n = min(a for i, a in enumerate(a))
+    n = a.index(n)
+    if n == 9:
+        print(a)
+    else:
+        a[n], a[9] = a[9], a[n]
+        print(a)
+
